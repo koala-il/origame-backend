98efdcf8ff1e2adbe6ba289fb49bce71a95bd67a
Dor <dor.b4r@gmail.com>
2020-04-23 19:09:32 +0300

    chore: Register the CRM models to the Admin

--- app/crm/admin.py
+++ app/crm/admin.py
@@ -1,1 +1,39 @@
-# Register your models here.
+from django.contrib import admin
+
+from crm.models import Customer, CustomerNote, ImageFile
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'first_name',
+        'last_name',
+        'phone',
+        'country',
+    ]
+    list_filter = ['user', 'country']
+
+
+@admin.register(ImageFile)
+class ImageFileAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'file',
+        'user',
+    ]
+
+    list_filter = ['user']
+
+
+@admin.register(CustomerNote)
+class CustomerNoteAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'customer',
+    ]
+
+    list_filter = ['customer', 'customer__user']
