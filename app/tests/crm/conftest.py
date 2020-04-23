f66681309815868e8da827e56b1c1167f12166f7
Dor <dor.b4r@gmail.com>
2020-04-23 19:10:08 +0300

    test: Full coverage for the users app models

--- app/tests/crm/conftest.py
+++ app/tests/crm/conftest.py
@@ -11,17 +11,21 @@ from users.models import CustomUser
 
 @pytest.fixture(scope="function")
 def add_image():
-    def _add_image(name, image_file_name):
+    def _add_image(name, image_file_name, user: CustomUser):
         image_path = os.path.join(
             settings.BASE_DIR, "tests", "crm", "assets", image_file_name
         )
         with open(image_path, "rb") as image_file:
             image_uploaded_file = SimpleUploadedFile(
-                name=name, content=image_file.read(), content_type="image/jpeg"
+                name=name,
+                content=image_file.read(),
+                content_type="image/jpeg"
             )
 
         image_file_object = ImageFile.objects.create(
-            title=name, file=image_uploaded_file,
+            user=user,
+            title=name,
+            file=image_uploaded_file,
         )
         return image_file_object
 
--- app/tests/users/test_models.py
+++ app/tests/users/test_models.py
@@ -6,22 +6,12 @@ from django.contrib.auth import get_user_model
 def test_create_user():
     User = get_user_model()
     user = User.objects.create_user(email="normal@user.com", password="foo")
+
     assert user.email == "normal@user.com"
+    assert str(user) == "normal@user.com"
     assert user.is_active is True
     assert user.is_staff is False
     assert user.is_superuser is False
-    try:
-        # username is None for the AbstractUser option
-        # username does not exist for the AbstractBaseUser option
-        assert user.username is None
-    except AttributeError:
-        pass
-    # with self.assertRaises(TypeError):
-    #     User.objects.create_user()
-    # with self.assertRaises(TypeError):
-    #     User.objects.create_user(email='')
-    # with self.assertRaises(ValueError):
-    #     User.objects.create_user(email='', password="foo")
 
 
 @pytest.mark.django_db
@@ -33,12 +23,53 @@ def test_create_superuser():
     assert admin_user.is_active is True
     assert admin_user.is_staff is True
     assert admin_user.is_superuser is True
+
+
+@pytest.mark.django_db
+def test_create_superuser_not_staff():
+    User = get_user_model()
+    admin_user = None
     try:
-        # username is None for the AbstractUser option
-        # username does not exist for the AbstractBaseUser option
-        assert admin_user.username is None
-    except AttributeError:
-        pass
-    # with self.assertRaises(ValueError):
-    #     User.objects.create_superuser(
-    #         email='super@user.com', password='foo', is_superuser=False)
+        admin_user = User.objects.create_superuser(
+            "super@user.com",
+            "foo",
+            is_staff=False
+        )
+    except ValueError as e:
+        assert isinstance(e, ValueError)
+        assert admin_user is None
+
+    assert User.objects.all().count() == 0
+
+
+@pytest.mark.django_db
+def test_create_superuser_not_superuser():
+    User = get_user_model()
+    admin_user = None
+    try:
+        admin_user = User.objects.create_superuser(
+            "super@user.com",
+            "foo",
+            is_superuser=False
+        )
+    except ValueError as e:
+        assert isinstance(e, ValueError)
+        assert admin_user is None
+
+    assert User.objects.all().count() == 0
+
+
+@pytest.mark.django_db
+def test_create_user_no_email():
+    User = get_user_model()
+    admin_user = None
+    try:
+        admin_user = User.objects.create_user(
+            None,
+            "foo"
+        )
+    except ValueError as e:
+        assert isinstance(e, ValueError)
+        assert admin_user is None
+
+    assert User.objects.all().count() == 0
