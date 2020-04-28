87740da06b1f68786daa89eddc15b016dd42c2ac
Dor <dor.b4r@gmail.com>
2020-04-28 12:59:38 +0300

    refactor: Use the default Swagger documentation for REST API

--- app/crm/views.py
+++ app/crm/views.py
@@ -1,6 +1,3 @@
-from django.contrib.auth import get_user_model
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -15,22 +12,9 @@ class CustomerView(ModelViewSet):
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
+        """
+        We want the user to get only the Customers associated with his account.
+        """
         user = self.request.user
 
-        if isinstance(user, get_user_model()):
-            return Customer.objects.filter(user=user).order_by("first_name")
-        else:
-            return Customer.objects.none()
-
-    @swagger_auto_schema(
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                "name": openapi.Schema(type=openapi.TYPE_STRING),
-                "phone": openapi.Schema(type=openapi.TYPE_STRING),
-                "email": openapi.Schema(type=openapi.TYPE_STRING),
-            },
-        )
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        return Customer.objects.filter(user=user).order_by("first_name")
